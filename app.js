777140c5f4ee8247eb11c058ba7ce6c5e1670851
younggyolee <younglee.work@gmail.com>
2020-05-04 14:26:30 +0900

    Add API endpoints for logged-in users

--- app.js
+++ app.js
@@ -71,14 +71,16 @@ app.use(function(req, res, next) {
 
 // error handler
 app.use(function(err, req, res, next) {
-  console.log(err);
   // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+  const message = err.message;
+  const error = req.app.get('env') === 'development' ? err : {};
+  console.error(message, error);
 
   // render the error page
   res.status(err.status || 500);
-  res.render('error');
+  res.json({
+    result: 'Error'
+  });
 });
 
 module.exports = app;
--- routes/controllers/api.controller.js
+++ routes/controllers/api.controller.js
@@ -2,10 +2,141 @@ const ebayApi = require('../utils/ebayApi');
 const googleTranslateApi = require('../utils/googleTranslateApi');
 const { Client } = require('pg');
 const { v4: uuidv4 } = require('uuid');
+const moment = require('moment');
 
 const SIGNUP_TYPES = {
   EMAIL: 'email',
   GUEST: 'guest'
+};
+
+exports.addGuestItem = async function(req, res, next) {
+  try {
+    await addItem(req.sessionID, req.params.keyword, SIGNUP_TYPES.GUEST);
+    res.status(200).end();
+  } catch (err) {
+    console.log('Error while adding guest item.\n', err);
+    res.status(500).json({
+      result: 'error',
+      error: {
+        'message': 'Internal Server Error while getting average price for a keyword'
+      }
+    });
+    next(err);
+  }
+};
+
+exports.addUserItem = async function(req, res, next) {
+  try {
+    await addItem(req.user.id, req.params.keyword, SIGNUP_TYPES.EMAIL);
+    res.status(200).json({
+      result: 'ok'
+    });
+  } catch (err) {
+    console.log('Error while adding guest item.\n', err);
+    res.status(500).json({
+      result: 'error',
+      error: {
+        'message': 'Internal Server Error while getting average price for a keyword'
+      }
+    });
+    next(err);
+  }
+};
+
+exports.getGuestItems = async function(req, res, next) {
+  try {
+    const client = new Client();
+    await client.connect();
+    const items = await getItems(client, req.sessionID);
+    const totalValue = await getTotalValue(client, req.sessionID);
+    await client.end();
+    res.status(200).json({
+      items,
+      totalValue
+    });
+  } catch (err) {
+    res.status(500).json({
+      result: 'error',
+      error: {
+        'message': 'Internal Server Error while getting items'
+      }
+    });
+    next(err);
+  }
+};
+
+exports.getUserItems = async function(req, res, next) {
+  try {
+    const client = new Client();
+    await client.connect();
+    const items = await getItems(client, req.user.id);
+    const totalValue = await getTotalValue(client, req.user.id);
+    await client.end();
+    res.status(200).json({
+      items,
+      totalValue
+    });
+  } catch (err) {
+    res.status(500).json({
+      result: 'error',
+      error: {
+        'message': 'Internal Server Error while getting items'
+      }
+    });
+    next(err);
+  }
+};
+
+exports.deleteGuestItem = async function(req, res, next) {
+  try {
+    const client = new Client();
+    await client.connect();
+    await deleteItem(client, req.params.itemId);
+    await client.end();
+    res.status(200).end();
+  } catch (err) {
+    console.log('Error while deleting guest item.\n', err);
+    next(err);
+  }
+};
+
+exports.deleteUserItem = async function(req, res, next) {
+  try {
+    const client = new Client();
+    await client.connect();
+    await deleteItem(client, req.userId);
+    await client.end();
+    res.status(200).end();
+  } catch (err) {
+    console.log('Error while deleting guest item.\n', err);
+    next(err);
+  }
+};
+
+exports.updateGuestItem = async function(req, res, next) {
+  try {
+    const client = new Client();
+    await client.connect();
+    if (req.body.type === 'price') {
+      await updateItemPrice(
+        client,
+        req.params.itemId,
+        req.body.price,
+        req.body.priceCurrency
+      );
+    } else if (req.body.type === 'imageURL') {
+      await updateItemImageURL(
+        client,
+        req.params.itemId,
+        req.body.imageURL
+      );
+    }
+    await client.end();
+    res.status(200).end();
+  } catch (err) {
+    console.log('Error while updating guest item.\n', err);
+    next(err);
+  }
 };
 
 async function addItem(userId, keyword, signup_type) {
@@ -35,225 +166,128 @@ async function addItem(userId, keyword, signup_type) {
     }
   }
 
+  const itemId = uuidv4();
   await saveItemToDB(
     client,
-    uuidv4(),
+    itemId,
     userId,
     keyword,
     avgPrice,
     currency,
     imageURL
   );
+  await saveListingsToDB(client, itemId, listings);
   await client.end();
+}
 
-  async function saveItemToDB(client, id, userId, keyword, price, currency, imageURL) {
-    const now = new Date().toISOString();
+async function saveListingsToDB(client, itemId, listings) {
+  for (listing of listings) {
     await client.query(
-      `INSERT INTO items (
+      `INSERT INTO listings (
         id,
-        user_id,
+        item_id,
+        end_date,
         title,
-        description,
-        category_id,
-        category_name,
+        source,
         price,
-        price_currency,
-        price_last_update_time,
-        creation_time,
-        image_url,
-        is_owned
+        price_currency_code,
+        url,
+        image_url
       )
       VALUES (
-        '${id}',
-        '${userId}',
-        '${keyword}',
-        '', -- description
-        '', -- category_id
-        '', -- category_name
-        ${ price ? `${price}` : 'NULL' },
-        '${currency}',
-        '${now}',
-        '${now}',
-        '${imageURL}',
-        true
+        '${uuidv4()}',
+        '${itemId}',
+        '${listing.endTime}',
+        '${listing.title}',
+        'ebay',
+        ${listing.price},
+        '${listing.priceCurrency}',
+        '${listing.url}',
+        '${listing.imageURL}'
       );`
     );
   }
 }
 
-exports.addGuestItem = async function(req, res, next) {
-  try {
-    await addItem(req.sessionID, req.params.keyword, SIGNUP_TYPES.GUEST);
-//     // Save guest user to [users] table, if not exist
-//     const client = new Client();
-//     await client.connect();
-//     await client.query(
-//       `INSERT INTO users
-//        VALUES ('${req.sessionID}', NULL, NULL, 'guest')
-//        ON CONFLICT DO NOTHING;`
-//     );
 
-//     // Get average price and related listings from eBay API
-//     const translatedKeyword = await googleTranslateApi.translate('en', req.params.keyword);
-//     const { listings, currency } = await ebayApi.getListings(translatedKeyword);
+async function saveItemToDB(client, id, userId, keyword, price, currency, imageURL) {
+  const now = new Date().toISOString();
+  await client.query(
+    `INSERT INTO items (
+      id,
+      user_id,
+      title,
+      description,
+      category_id,
+      category_name,
+      price,
+      price_currency,
+      price_last_update_time,
+      creation_time,
+      image_url,
+      is_owned
+    )
+    VALUES (
+      '${id}',
+      '${userId}',
+      '${keyword}',
+      '', -- description
+      '', -- category_id
+      '', -- category_name
+      ${ price ? `${price}` : 'NULL' },
+      '${currency}',
+      '${now}',
+      '${now}',
+      '${imageURL}',
+      true
+    );`
+  );
+}
 
-//     // calculate avg price
-//     const sum = listings.reduce((accumulator, item) => {
-//       return accumulator + Number(item.price);
-//     }, 0);
-//     const price = Math.round(sum / listings.length);
 
-//     // get image URL
-//     let imageURL = '';
-//     for (let i = 0; i < listings.length; i++) {
-//       if (listings[i].imageURL) {
-//         imageURL = listings[i].imageURL;
-//         break;
-//       }
-//     }
+async function getItems(client, userId) {
+  const { rows } = await client.query(
+    `SELECT id,
+            title,
+            price,
+            price_currency AS "priceCurrency",
+            image_url AS "imageURL",
+            creation_time AS "creationTime"
+     FROM items
+     WHERE user_id='${userId}'
+     ORDER BY creation_time`
+  );
+  return rows;
+}
 
-//     // Save the item to DB
-//     await client.query(
-//       `INSERT INTO items (
-//         id,
-//         user_id,
-//         title,
-//         description,
-//         category_id,
-//         category_name,
-//         price,
-//         price_currency,
-//         price_last_update_time,
-//         creation_time,
-//         image_url,
-//         is_owned
-//       )
-//       VALUES (
-//         '${uuidv4()}',
-//         '${req.sessionID}',
-//         '${req.params.keyword}',
-//         '', -- description
-//         '', -- category_id
-//         '', -- category_name
-//         ${ price ? `${price}` : 'NULL' },
-//         '${currency}',
-//         '${new Date().toISOString()}',
-//         '${new Date().toISOString()}',
-//         '${imageURL}',
-//         true
-//       );`
-//     );
-//     await client.end();
-    res.status(200).end();
-  } catch (err) {
-    console.log('Error while adding guest item.\n', err);
-    res.status(500).json({
-      result: 'error',
-      error: {
-        'message': 'Internal Server Error while getting average price for a keyword'
-      }
-    });
-    next(err);
-  }
-};
+async function getTotalValue(client, userId) {
+  const { rows } = await client.query(
+    `SELECT SUM(price) AS sum
+    FROM items
+    WHERE user_id = '${userId}'`
+  );
+  return rows[0].sum;
+}
 
-exports.getGuestItems = async function(req, res, next) {
-  try {
-    const client = new Client();
-    await client.connect();
-    const items = await client.query(
-      `SELECT id,
-              title,
-              price,
-              price_currency AS "priceCurrency",
-              image_url AS "imageURL",
-              creation_time AS "creationTime"
-       FROM items
-       WHERE user_id='${req.sessionID}'
-       ORDER BY creation_time`
-    );
-    const totalValue = await client.query(
-      `SELECT SUM(price) AS sum
-       FROM items
-       WHERE user_id = '${req.sessionID}'`
-    );
-    // console.log(totalValue.rows[0].sum);
-    await client.end();
-    res.status(200).json({
-      items: items.rows,
-      totalValue: totalValue.rows[0].sum
-    });
-  } catch (err) {
-    res.status(500).json({
-      result: 'error',
-      error: {
-        'message': 'Internal Server Error while getting items'
-      }
-    });
-    next(err);
-  }
-};
+async function deleteItem(client, itemId) {
+  await client.query(
+    `DELETE FROM items WHERE id='${itemId}'`
+  );
+}
 
-exports.deleteGuestItem = async function(req, res, next) {
-  try {
-    const client = new Client();
-    await client.connect();
-    await client.query(
-      `DELETE FROM items WHERE id='${req.params.itemId}'`
-    );
-    await client.end();
-    res.status(200).end();
-  } catch (err) {
-    // res.status(500).end();
-    console.log('Error while deleting guest item.\n', err);
-    next(err);
-  }
-};
+async function updateItemPrice(client, itemId, price, priceCurrency) {
+  await client.query(
+    `UPDATE items
+      SET price=${price},
+          price_currency='${priceCurrency}'
+      WHERE id='${itemId}'`
+  );
+}
 
-exports.updateGuestItem = async function(req, res, next) {
-  console.log('updating guest item price');
-  try {
-    const client = new Client();
-    await client.connect();
-    if (req.body.type === 'price') {
-      await client.query(
-        `UPDATE items
-         SET price=${req.body.price},
-             price_currency='${req.body.priceCurrency}'
-         WHERE id='${req.params.itemId}'`
-      );
-    } else if (req.body.type === 'imageURL') {
-      const client = new Client();
-      await client.connect();
-      await client.query(
-        `UPDATE items
-         SET image_url='${req.body.imageURL}'
-         WHERE id='${req.params.itemId}'`
-      );
-    }
-    await client.end();
-    res.status(200).end();
-  } catch (err) {
-    console.log('Error while updating guest item.\n', err);
-    next(err);
-  }
-};
-
-exports.addUserItem = async function(req, res, next) {
-  try {
-    await addItem(req.user.id, keyword, SIGNUP_TYPES.EMAIL);
-  } catch (err) {
-    console.log('Error while adding guest item.\n', err);
-    res.status(500).json({
-      result: 'error',
-      error: {
-        'message': 'Internal Server Error while getting average price for a keyword'
-      }
-    });
-    next(err);
-  }
-};
-
-exports.getUserItems = async function(req, res, next) {
-
-};
+async function updateItemImageURL(client, itemId, imageURL) {
+  await client.query(
+    `UPDATE items
+     SET image_url='${imageURL}'
+     WHERE id='${itemId}'`
+  );
+}
