bd4721fa2f3b9bb410b984488abbcf82af5d1b43
EC2 Default User <ec2-user@ip-172-31-26-135.us-west-1.compute.internal>
2020-05-07 08:21:37 +0000

    update Bingbot for linux environment

--- routes/utils/bingBot.js
+++ routes/utils/bingBot.js
@@ -3,24 +3,25 @@ const puppeteer = require('puppeteer');
 exports.getImageForKeyword = async function(keyword) {
   console.log('bing bot turned on!');
   const browser = await puppeteer.launch({
-    headless: false
+  //  headless: false
   });
   const page = await browser.newPage();
-  await page.goto('https://www.bing.com/image', { waitUntil: 'networkidle2' });
+  await page.goto('https://www.bing.com/?scope=images', { waitUntil: 'networkidle2' });
 
   await page.type('#sb_form_q', keyword);
-
-  const searchButtonSelector = 'label[for="sb_form_go"]';
+  await page.screenshot({path: 'bing0.png'});
+  const searchButtonSelector = 'input[id="sb_form_go"]';
   await page.waitForSelector(searchButtonSelector);
   await page.click(searchButtonSelector);
 
   const imageSelector = '.mimg';
-  await page.waitFor(2000 + Math.random() * 1000);  
+  await page.waitFor(2000 + Math.random() * 1000);
+  await page.screenshot({path: 'bing1.png'});
   const images = await page.$$eval(
     imageSelector,
     imgs => imgs.map(img => img.getAttribute('src'))
   );
-  console.log(images);
+
   await browser.close();
   return images[0];
 };
