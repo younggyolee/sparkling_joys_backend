0fa06ee8229246b972ee11ee1dfefb63cbf1a038
younggyolee <younglee.work@gmail.com>
2020-05-08 13:35:32 +0900

    Add logic to close browser into bingBot

--- routes/utils/bingBot.js
+++ routes/utils/bingBot.js
@@ -1,37 +1,41 @@
 const puppeteer = require('puppeteer');
 
 exports.getImageForKeyword = async function(keyword) {
-  console.log('bing bot turned on!');
-  const browser = await puppeteer.launch({
-    // headless: false
-  });
-  const page = await browser.newPage();
-  await page.goto('https://www.bing.com/?scope=images', { waitUntil: 'networkidle0' });
+  const browser = await puppeteer.launch();
+  try {
+    const page = await browser.newPage();
+    console.log('bing bot turned on!');
+    await page.goto('https://www.bing.com/?scope=images', { waitUntil: 'networkidle0' });
 
-  await page.type('#sb_form_q', keyword);
-  let searchButtonSelector;
-  if (process.env.NODE_ENV === 'development') {
-    searchButtonSelector = 'label[for="sb_form_go"]';
-  } else {
-    searchButtonSelector = 'input[id="sb_form_go"]';
+    await page.type('#sb_form_q', keyword);
+    let searchButtonSelector;
+    if (process.env.NODE_ENV === 'development') {
+      searchButtonSelector = 'label[for="sb_form_go"]';
+    } else {
+      searchButtonSelector = 'input[id="sb_form_go"]';
+    }
+    await page.waitForSelector(searchButtonSelector);
+    await page.click(searchButtonSelector);
+
+    const imageLinksSelector = 'a[class="iusc"]';
+    await page.waitFor(1000 + Math.random() * 2000);
+    const links = await page.$$eval(
+      imageLinksSelector,
+      linkTags => linkTags.map(linkTag => linkTag.getAttribute('href'))
+    );
+
+    await page.goto('https://bing.com' + links[0], { waitUntil: 'networkidle0' });
+    const imageSelector='img.nofocus'
+    await page.waitForSelector(imageSelector);
+    const images = await page.$$eval(
+      imageSelector,
+      imgs => imgs.map(img => img.getAttribute('src'))
+    );
+    browser.close();
+
+    return images[0];
+  } catch (err) {
+    console.log('Error while getting image using BingBot.\n', err);
+    browser.close();
   }
-  await page.waitForSelector(searchButtonSelector);
-  await page.click(searchButtonSelector);
-
-  const imageLinksSelector = 'a[class="iusc"]';
-  await page.waitFor(1000 + Math.random() * 2000);
-  const links = await page.$$eval(
-    imageLinksSelector,
-    linkTags => linkTags.map(linkTag => linkTag.getAttribute('href'))
-  );
-
-  await page.goto('https://bing.com' + links[0], { waitUntil: 'networkidle0' });
-  const imageSelector='img.nofocus'
-  await page.waitForSelector(imageSelector);
-  const images = await page.$$eval(
-    imageSelector,
-    imgs => imgs.map(img => img.getAttribute('src'))
-  );
-
-  return images[0];
 };
