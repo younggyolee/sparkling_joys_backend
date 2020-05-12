d76f9de9e46b577336120dcb2cf3c5c5cb2fe6ac
younggyolee <younglee.work@gmail.com>
2020-05-12 23:58:18 +0900

    Add response for '/' for loadbalancer ping

--- app.js
+++ app.js
@@ -56,6 +56,11 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+app.use('/', (req,res) => {
+  res.status(200).json({
+    result: 'ok'
+  });
+})
 app.use('/api', apiRouter);
 app.use('/signup', signupRouter);
 app.use('/login', loginRouter);
